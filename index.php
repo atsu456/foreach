5e9d12a98af309ade7b5c19bc43e25e9066d4d24
{kataoka} <{atatatuuu456@gmail.com}>
2023-06-11 20:57:16 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://use.typekit.net/pke3ujd.css">
+    <script src="https://kit.fontawesome.com/0fb73e8725.js" crossorigin="anonymous"></script>
+</head>
+
+<body id="top">
+    <div id="container">
+
+        <header class="header">
+            <h1 class="header__logo"><a href="#"><img src="image/camplogo.svg" alt="foreach campground"></a></h1>
+            <nav id="g-nav">
+                <ul class="nav">
+                    <li class="g-nav__item"><a href="#">予約</a></li>
+                    <li class="g-nav__item"><a href="#">オンラインショップ</a></li>
+                </ul>
+            </nav>
+        </header>
+
+        <main>
+            <article>
+
+                <section id="mv">
+                    <div class="mv__text">
+                        <h1 class="mv__copy">福岡にあるリゾート融合体</h1>
+                        <p class="mv__lead">QUIET PLACE<br>AND THE<br>STARRY SKY</p>
+                    </div>
+                </section>
+
+                <section id="news" class="contents">
+                    <div class="news_page_description">
+                        <h2 class="title-style">
+                            01
+                            <p class="title-border-right">NEWS</p>
+                        </h2>
+                        <p>イベント情報など<br>最新の情報をお届けいたします。</p>
+                        <a href="#" class="btn">READ MORE</a>
+                    </div>
+                    <ul class="news__item">
+                        <li><a href="#">
+                                <figure class="img"><img src="image/rantan.jpg" alt="ランタン"></figure>
+                            </a></li>
+                        <li><a href="#">
+                                <figure class="img"><img src="image/cup.jpg" alt="マグカップ"></figure>
+                            </a></li>
+                        <li><a href="#">
+                                <figure class="img"><img src="image/fire.jpg" alt="焚き火"></figure>
+                            </a></li>
+                    </ul>
+                </section>
+
+                <section id="facility" class="contents">
+                    <div class="page_description right_flow">
+                        <h2 class="title-style">
+                            02
+                            <p class="title-border-left">FACILITY</p>
+                        </h2>
+                        <p>サイトや料金など、<br>快適にキャンプをお楽しみ頂くための<br>施設をご紹介いたします。</p>
+                        <a href="#" class="btn btn_r">READ MORE</a>
+                    </div>
+                    <figure class="contents_img"><img src="image/tent1.jpg" alt="テント"></figure>
+                </section>
+                
+                <section id="store" class="contents">
+                    <div class="page_description">
+                        <h2 class="title-style">
+                            03
+                            <p class="title-border-right">STORE</p>
+                        </h2>
+                        <p>キャンプギア、アパレル用品の購入はこちらから！<br>限定商品も取り扱っていますので<br>是非ご覧ください。</p>
+                        <a href="#" class="btn">READ MORE</a>
+                        </div>
+                        <figure class="contents_img"><img src="image/mug.jpg" alt="マグカップ"></figure>
+                </section>
+
+                <section id="access" class="contents">
+                    <div class="page_description right_flow">
+                        <h2 class="title-style">
+                            04
+                            <p class="title-border-left">ACCESS</p>
+                        </h2>
+                        <p>foreach camp ground<br>〒888-8888<br>福岡県福岡市東区888-88<br>0493-81-6166</p>
+                        <a href="#" class="btn btn_r">READ MORE</a>
+                        </div>
+                        <figure class="contents_img">googlemap</figure>
+                </section>
+
+            </article>
+        </main>
+
+        <footer class="footer">
+        <div class="ft">
+        <ul class="ft__ul">
+        <li class="ft__logo"><a href="#"><img src="image/camplogo.svg" alt="foreach campground"></a></li>
+        <li class="ft__add"><span class="ft__name">foreach camp ground</span></li>
+        <li class="ft__add">〒888-8888</li>
+        <li class="ft__add">福岡県福岡市東区888-88</li>
+        <li class="ft__add">0493-81-6166</li>
+        </ul>
+            <ul class="ft_links_ul">
+                <li class="ft_links_li"><a href="#">アクセス</a></li>
+                <li class="ft_links_li"><a href="#">お知らせ</a></li>
+                <li class="ft_links_li"><a href="#">施設紹介</a></li>
+                <li class="ft_links_li"><a href="#">予約</a></li>
+                <li class="ft_links_li"><a href="#">オンラインストア</a></li>
+            </ul>
+            <ul class="ft_links_ul">
+                <li class="ft_links_li"><a href="#">会社概要・拠点情報</a></li>
+                <li class="ft_links_li"><a href="#">事業情報</a></li>
+                <li class="ft_links_li"><a href="#">採用情報</a></li>
+                <li class="ft_links_li"><a href="#">個人情報保護方針</a></li>
+                <li class="ft_links_li"><a href="#">ソーシャルメディアポリシー</a></li>
+            </ul>
+            </div>
+            <div class="ft_snsLinks">
+                <ul class="ft_snsLinks_ul">
+                    <li><a href="#"><i class="fa-brands fa-square-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa-brands fa-square-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa-brands fa-square-instagram"></i></a></li>
+                </ul>
+            </div>
+            <div class="ft_copyright">©2023 foreach campground</div>
+        </footer>
+</body>
+
+</html>
