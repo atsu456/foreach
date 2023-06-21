2d83418e96c18558d446118e035560389d31ad8e
taniyama0922 <136245683+taniyama0922@users.noreply.github.com>
2023-06-21 23:31:32 +0900

    Add files via upload

---
+++ facility.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://use.typekit.net/pke3ujd.css">
+    <script src="https://kit.fontawesome.com/0fb73e8725.js" crossorigin="anonymous"></script>
+</head>
+
+<body id="facility">
+    <div id="container">
+
+        <header class="header">
+            <h1 class="header__logo"><a href="index.php"><img src="image/camplogo.svg" alt="foreach campground"></a></h1>
+            <nav id="g-nav">
+                <ul class="nav">
+                    <li class="g-nav__item"><a href="#">予約</a></li>
+                    <li class="g-nav__item"><a href="#">オンラインショップ</a></li>
+                </ul>
+            </nav>
+        </header>
+
+        <main>
+            <article>
+
+                <section id="mv">
+                    <div class="mv__text">
+                        <h1 class="mv__copy">FACILITY</h1>
+                    </div>
+                </section>
+                <section id="copy" class="contents">
+                    自然の中で味わう「非日常」。四季や天気の変わり目とともに、地元の食材を味わう。<br>
+                    日が暮れて、火を囲み、素敵なテントで眠りにつく。居心地よく、美しく、自然に寄り添うだけの１日。<br>
+                    日常とはちがうもうひとつの時間を過ごしてみませんか。
+                </section>
+
+                <section id="plan">
+                    <table class="Plan">
+                        <caption>Usage fee / Plan<br>ご利用料金・宿泊プラン</caption>
+                        <tr>
+                            <th>サイト</th>
+                            <th>料金<br>※税込価格</th>
+                            <th>区画数</th>
+                        </tr>
+                        <tr>
+                            <td>フリーサイト<br>電源なし</td>
+                            <td>¥7,150</td>
+                            <td>25<br>1サイト6名まで<br>車1台<br>テント・タープ1張りまで</td>
+                        </tr>
+                        <tr>
+                            <td>区画電源オートサイト<br>電源あり</td>
+                            <td>¥8,250</td>
+                            <td>25<br>1サイト6名まで<br>車1台<br>テント・タープ1張りまで</td>
+                        </tr>
+                        <tr>
+                            <td>ソロキャンプサイト<br>電源なし</td>
+                            <td>¥4,400</td>
+                            <td>15<br>1サイト4名まで<br>車1台<br>テント1張りまで</td>
+                        </tr>
+                    </table>
+                    <ul>
+                        <li>※チェックイン時間に遅れた場合の返金はありません。</li>
+                        <li>※レイトチェックアウトは行っておりません。</li>
+                        <li>※ゴミ処理料金・シャワールーム使用料金を含んでおります。</li>
+                    </ul>
+                </section>
+                <section id="rental">
+                    <table class="rental">
+                        <caption>Rental Service<br>レンタルサービス</caption>
+                        <tr>
+                            <th>ギア</th>
+                            <th>付属品/備考</th>
+                            <th>料金<br>※税込価格</th>
+                        </tr>
+                        <tr>
+                            <td>テントS<br>(最大4名)</td>
+                            <td>グランドシート<br>マットレス<br>ペグ、ペグハンマー</td>
+                            <td>¥5,500</td>
+                        </tr>
+                        <tr>
+                            <td>テントL<br>最大6名</td>
+                            <td>グランドシート<br>マットレス<br>ペグ、ペグハンマー</td>
+                            <td>¥7,700</td>
+                        </tr>
+                        <tr>
+                            <td>タープ</td>
+                            <td>ペグ、ペグハンマー</td>
+                            <td>¥5,500</td>
+                        </tr>
+                        <tr>
+                            <td>テーブル</td>
+                            <td>6人がけ</td>
+                            <td>¥3,300</td>
+                        </tr>
+                        <tr>
+                            <td>チェア</td>
+                            <td>1脚</td>
+                            <td>¥1,100</td>
+                        </tr>
+                        <tr>
+                            <td>焚火台</td>
+                            <td>火ばさみ</td>
+                            <td>¥2,750</td>
+                        </tr>
+                        <tr>
+                            <td>シュラフ</td>
+                            <td>フトンタイプ ワイド</td>
+                            <td>¥3,300</td>
+                        </tr>
+                    </table>
+                </section>
+                <section id="service">
+                    <p class="sec_terms">Terms of Service<br><span>ご利用規約</span></p>
+                    <p class="el_lv5Hd">スノーピークキャンプフィールド<br class="hp_brSp">ご利用規約</hp>
+                    <p class="el_pg el_pg__sMin">営業期間および営業時間 、ご利用にあたっての規約を以下のとおりと致します。</p>
+    </div>
+    <div class="bl_tableBasic_table">
+        <table>
+            <tbody>
+                <tr>
+                    <th>
+                        <h4 class="el_lv6Hd">営業期間</h4>
+                    </th>
+                    <td>
+                        <p class="el_pg el_pg__wid">通年</p>
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        <h4 class="el_lv6Hd">営業時間</h4>
+                    </th>
+                    <td>
+                        <p class="el_pg el_pg__lBld">10:00 ～ 18:00</p>
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        <h4 class="el_lv6Hd">宿泊</h4>
+                    </th>
+                    <td>
+                        <p class="el_pg el_pg__wid">チェックイン：12:00～18:00<br>チェックアウト：～11:00</p>
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        <h4 class="el_lv6Hd">受付方法</h4>
+                    </th>
+                    <td>
+                        <p class="el_pg el_pg__wid">当キャンプ場は、「完全予約制」を採用しております。<br>
+                            予約フォームもしくはお電話にて承っております。<br>
+                            チェックインの際は、予約完了メールに記載のURLからダウンロードしたQRコードの提示をお願いいたします。<br>
+                            予約状況によっては、当日受付も可能です。当日受付の場合はストアまでご連絡ください。（電話番号：0493-81-6166）
+                        </p>
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        <h4 class="el_lv6Hd">禁止事項</h4>
+                    </th>
+                    <td>
+                        <p class="el_pg el_pg__wid">フィールド内では、安全で快適なキャンプライフを送れるよう、次の事項に該当する行為は、原則禁止としております。</p>
+                        <ul class="el_listBasic el_pg el_pg__wid">
+                            <li>ペットのご入場はお断りしております。ご了承ください。</li>
+                            <li>消灯時間以降（22時～7時）における他のお客さまへの迷惑行為</li>
+                            <li>ドローンの飛行(サイズにかかわらず禁止致します)</li>
+                            <li>直火による焚火</li>
+                            <li>サイト内での喫煙行為</li>
+                            <li>花火の使用（手持ち花火も含む）</li>
+                            <li>カラオケ、楽器の使用</li>
+                            <li>発電機の使用</li>
+                            <li>ゴミの放棄</li>
+                            <li>指定時間外の車両の入退場</li>
+                            <li>駐停車中のアイドリング</li>
+                            <li>管理者の許可なく、利用申込者以外のものを場内へ招き入れる</li>
+                            <li>管理者の許可を受けずに、物販、広告物を配布する行為</li>
+                            <li>法令の規定や公序良俗に反する行為</li>
+                            <li>大きな声や音を発生させ、他の利用者に著しく迷惑をかけること</li>
+                            <li>その他管理者の指定する行為</li>
+                        </ul>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        </section>
+        <section id="map">
+            <p class="map">Campfield Map<br>施設案内</p>
+            <!-- <img src="" alt=""> -->
+            <div class="facilityList">
+                <div class="list">
+                    <p>ストア・管理棟</p>
+                    <p>ストアではキャンプギア・アパレルの販売を行っています。ご滞在中にご不明な点等がございましたらお気軽にストアまでお越しください。</p>
+                </div>
+                
+                <div class="list">
+                <p>売店</p>
+                <p>薪や氷、調味料、消耗品等の商品を取り扱っています。</p>
+                </div>
+
+                <div class="list">
+                <p>シャワー・トイレ</p>
+                <p>24時間どなたでもご利用いただけるシャワーを完備しています。シャンプー・ボディソープ等は各自ご用意ください。サニタリー棟のトイレは暖房便座と温水洗浄を完備しています。</p>
+                </div>
+
+                <div class="list">
+                <p>炊事棟・洗い場</p>
+                <p>サニタリー棟にある炊事場兼洗い場は、水、温水ともご自由にご利用いただけます。</p>
+                </div>
+
+            </div>
+        </section>
+
+
+        </article>
+        </main>
+
+        <footer class="footer">
+            <div class="ft">
+                <ul class="ft__ul">
+                    <li class="ft__logo"><a href="index.php"><img src="image/camplogo.svg" alt="foreach campground"></a></li>
+                    <li class="ft__add"><span class="ft__name">foreach camp ground</span></li>
+                    <li class="ft__add">〒888-8888</li>
+                    <li class="ft__add">福岡県福岡市東区888-88</li>
+                    <li class="ft__add">0493-81-6166</li>
+                </ul>
+                <ul class="ft_links_ul">
+                    <li class="ft_links_li"><a href="#">アクセス</a></li>
+                    <li class="ft_links_li"><a href="#">お知らせ</a></li>
+                    <li class="ft_links_li"><a href="facility.php">施設紹介</a></li>
+                    <li class="ft_links_li"><a href="#">予約</a></li>
+                    <li class="ft_links_li"><a href="#">オンラインストア</a></li>
+                </ul>
+                <ul class="ft_links_ul">
+                    <li class="ft_links_li"><a href="#">会社概要・拠点情報</a></li>
+                    <li class="ft_links_li"><a href="#">事業情報</a></li>
+                    <li class="ft_links_li"><a href="#">採用情報</a></li>
+                    <li class="ft_links_li"><a href="#">個人情報保護方針</a></li>
+                    <li class="ft_links_li"><a href="#">ソーシャルメディアポリシー</a></li>
+                </ul>
+            </div>
+            <div class="ft_snsLinks">
+                <ul class="ft_snsLinks_ul">
+                    <li><a href="#"><i class="fa-brands fa-square-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa-brands fa-square-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa-brands fa-square-instagram"></i></a></li>
+                </ul>
+            </div>
+            <div class="ft_copyright">©2023 foreach campground</div>
+        </footer>
+</body>
+
+</html>
