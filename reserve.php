45f62d5038da008e00803139c83c291311e2246b
{kataoka} <{atatatuuu456@gmail.com}>
2023-06-22 23:14:43 +0900

    候補

--- reserve.php
+++ reserve.php
@@ -30,77 +30,125 @@
             </section>
 
             <div class="section_reserve contents">
-                
-                <div class="section_left">
-                    <h1>プラン一覧</h1>
-                    <div class="campsite_article">
-                        
-                        <div class="campsite_top">
-                        <img src="image/freesite.jpg" alt="" class="site_img">
-                            <div>
-                                <h2>フリーサイト(電源なし)</h2>
-                            </div>
-                        <P>¥7,150/拍(税込み)</P>
-                        <p>1~6名</p>
-                        <p>車１台</p>
-                        <p>テント・タープ１張りまで</p>
-                        </div>
-                        
-                        <div class="text">
-                        <p>フリーサイト(電源なし)は、キャンプ場内のエリアで、電気や電源が提供されていないキャンプスペースのことです。キャンプ場内の他の設備やアメニティは利用できますが、電力を必要とするデバイスや設備の使用は制限される可能性があります。電源なしフリーサイトを利用する場合は、自分自身で必要な設備やライティング、充電などの準備をする必要があります。自然の中でシンプルなキャンプ体験を楽しみたい方におすすめのスペースです。</p>
-                        </div>
-                        <div class="button_reserve">
-                            <a href="reserve_form.php">予約手続きへ進む</a>
-                        </div>
+  <div class="section_left">
+    <h1>プラン一覧</h1>
+    <div class="campsite_article">
+      <div class="campsite_top">
+        <img src="image/freesite.jpg" alt="" class="site_img">
+        <div>
+          <h2>フリーサイト(電源なし)</h2>
+        </div>
+        <p>¥7,150/拍(税込み)</p>
+        <p><i class="fas fa-users"></i> 1~6名</p>
+        <p><i class="fas fa-car"></i> 車１台</p>
+        <p><i class="fas fa-campground"></i> テント・タープ１張りまで</p>
+      </div>
+      <div class="text">
+        <p>フリーサイト(電源なし)は、キャンプ場内のエリアで、電気や電源が提供されていないキャンプスペースのことです。キャンプ場内の他の設備やアメニティは利用できますが、電力を必要とするデバイスや設備の使用は制限される可能性があります。電源なしフリーサイトを利用する場合は、自分自身で必要な設備やライティング、充電などの準備をする必要があります。自然の中でシンプルなキャンプ体験を楽しみたい方におすすめのスペースです。</p>
+      </div>
+      <div class="button_reserve">
+        <a href="reserve_form.php">予約手続きへ進む</a>
+      </div>
+    </div>
 
-                    </div>
-                    <div class="campsite_article">
-                        
-                        <div class="campsite_top">
-                        <img src="image/autosite.jpg" alt="" class="site_img">
-                                <div>
-                                    <h2>区画電源オートサイト(電源あり)</h2>
-                                </div>
-                                <P>¥8,250/拍(税込み)</P>
-                                <p>1~6名</p>
-                                <p>車１台</p>
-                                <p>テント・タープ１張りまで</p>
-                        </div>
-                        
-                        
-                        <div class="text">
-                            <p>区画電源オートサイト(電源あり)は、キャンプ場内に設けられたエリアで、テントやキャンピングカーを自由に設置することができます。このエリアには電源があり、電気を利用することができます。電源を利用することで、照明や冷蔵庫、電気ストーブなどの電化製品を使うことができます。電源あり区画フリーサイトは、キャンプ場での快適な滞在を求める人々におすすめのスペースです。</p>
-                        </div>
-                        <div class="button_reserve">
-                            <a href="reserve_form.php">予約手続きへ進む</a>
-                        </div>
+    <!-- Plan 2: 区画電源オートサイト(電源あり) -->
+    <div class="campsite_article">
+      <div class="campsite_top">
+        <img src="image/autosite.jpg" alt="" class="site_img">
+        <div>
+          <h2>区画電源オートサイト(電源あり)</h2>
+        </div>
+        <p>¥8,250/拍(税込み)</p>
+        <p><i class="fas fa-users"></i> 1~6名</p>
+        <p><i class="fas fa-car"></i> 車１台</p>
+        <p><i class="fas fa-campground"></i> テント・タープ１張りまで</p>
+      </div>
+      <div class="text">
+        <p>区画電源オートサイト(電源あり)は、キャンプ場内に設けられたエリアで、テントやキャンピングカーを自由に設置することができます。このエリアには電源があり、電気を利用することができます。電源を利用することで、照明や冷蔵庫、電気ストーブなどの電化製品を使うことができます。電源あり区画フリーサイトは、キャンプ場での快適な滞在を求める人々におすすめのスペースです。</p>
+      </div>
+      
+     
+<div class="button_reserve">
+        
+       
+<a href="reserve_form.php">予約手続きへ進む</a>
+      
+     
+</div>
+    
+   
+</div>
 
-                    </div>
-                    <div class="campsite_article">
-                        
-                        <div class="campsite_top">
-                        <img src="image/solosite.jpg" alt="" class="site_img">
-                            <div>
-                                <h2>ソロキャンプサイト(電源なし)</h2>
-                            </div>
-                        <P>¥4,400/拍(税込み)</P>
-                        <p>1~6名</p>
-                        <p>車１台</p>
-                        <p>テント・タープ１張りまで</p>
+    
 
-                        </div>
-                        
-                        <div class="text">
-                        <p>ソロキャンプサイト（電源なし）は、一人で利用することができるキャンプ場です。このキャンプ場では、電源が提供されていないため、自然の中でシンプルな生活を楽しむことができます。テントを張り、寝袋で寝泊まりし、焚火を囲んで食事をしたり、アウトドアアクティビティを楽しむことができます。電源がないため、携帯電話や電化製品の利用は制限されますが、自然の中での静寂な時間を過ごしたい方におすすめのスペースです。</p>
-                        </div>
-                        <div class="button_reserve">
-                            <a href="reserve_form.php">予約手続きへ進む</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="section_right">
-                    <h1>空室カレンダー</h1>
-                </div>
+   
+
+
+<!-- Plan 3: ソロキャンプサイト(電源なし) -->
+    
+   
+<div class="campsite_article">
+      
+     
+<div class="campsite_top">
+        
+       
+<img src="image/solosite.jpg" alt="" class="site_img">
+        <div>
+          
+         
+<h2>ソロキャンプサイト(電源なし)</h2>
+        
+       
+</div>
+        
+       
+<p>¥4,400/拍(税込み)</p>
+        
+       
+<p><i class="fas fa-users"></i> 1~6名</p>
+        
+       
+<p><i class="fas fa-car"></i> 車１台</p>
+        <p><i class="fas fa-campground"></i> テント・タープ１張りまで</p>
+      </div>
+      
+     
+<div class="text">
+        
+       
+<p>ソロキャンプサイト（電源なし）は、一人で利用することができるキャンプ場です。このキャンプ場では、電源が提供されていないため、自然の中でシンプルな生活を楽しむことができます。テントを張り、寝袋で寝泊まりし、焚火を囲んで食事をしたり、アウトドアアクティビティを楽しむことができます。電源がないため、携帯電話や電化製品の利用は制限されますが、自然の中での静寂な時間を過ごしたい方におすすめのスペースです。</p>
+      </div>
+      
+     
+<div class="button_reserve">
+        
+       
+<a href="reserve_form.php">予約手続きへ進む</a>
+      
+     
+</div>
+    
+   
+</div>
+  </div>
+
+  
+
+ 
+
+
+<div class="section_right">
+    
+   
+<h1>空室カレンダー</h1>
+    
+   
+<!-- Your calendar code can be added here -->
+  
+ 
+</div>
+</div>
         </article>
     </main>
     <footer class="footer">
