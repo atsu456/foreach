247a53ed9501e6a1373ff18f003a917ff6a60640
taniyama0922 <136245683+taniyama0922@users.noreply.github.com>
2023-06-25 23:17:23 +0900

    Update func.php

--- func/func.php
+++ func/func.php
@@ -1,129 +1,26 @@
 <?php
 
-// function e(string $str, string $charset = 'UTF-8'): string{
-//     return htmlspecialchars($str, ENT_QUOTES | ENT_HTML5, $charset, false);
-// }
+function e(string $str, string $charset = 'UTF-8'): string{
+    return htmlspecialchars($str , ENT_QUOTES | ENT_HTML5 , $charset, false);
+}
 
-// function getDb($dsn,$usr,$passwd) : PDO {
-
-//     // データベースの接続を確立
-//     $db = new PDO($dsn, $usr, $passwd);
-//     $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-//     return $db;
-// }
-// //ランダムな文字列の生成
-// function numbers($num){
-//     $chars = array();
-//     $chars = array_flip(
-//         array_merge(
-//             range('a','z'),range('A','Z'),range('0','9')
-//         )
-//     );
-//     $contact_number = '';
-//     for($i=0;$i<$num;$i++){
-//         $contact_number .= array_rand($chars);
-//     }
-//     return $contact_number;
-// }
-// 画像のデータベースに接続
-function connectDB() {
-    $param = 'mysql:dbname=foreach;host=localhost';
-    try {
-        $pdo = new PDO($param, 'root', 'root');
-        return $pdo;
-
-    } catch (PDOException $e) {
-        exit($e->getMessage());
+function getDb($dsn,$usr,$passwd) : PDO {
+    //データベースへの接続を確立
+    $db = new PDO($dsn, $usr, $passwd);
+    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    return $db;
+}
+//ランダムな文字列の生成
+function numbers($num){
+    $chars = array();
+    $chars = array_flip(
+        array_merge(
+            range('a','z'),range('A','Z'),range('0','9')
+        )
+    );
+    $contact_number = '';
+    for($i=0;$i<$num;$i++){
+        $contact_number .= array_rand($chars);
     }
+    return $contact_number;
 }
-
-//カート
-function h($str): string
-{
-  return htmlspecialchars($str, ENT_QUOTES, 'utf-8');
-}
-
-function getDB($dsn, $user, $pass): PDO
-{
-  $dbh = new PDO($dsn, $user, $pass);
-  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  return $dbh;
-}
-
-function numbers($num): string
-{
-  $chars = array();
-  $chars = array_flip(
-    array_merge(
-      range('a', 'z'),
-      range('A', 'Z'),
-      range('0', '9')
-    )
-  );
-  $contact_number = '';
-  for ($i = 0; $i < $num; $i++) {
-    $contact_number .= array_rand($chars);
-  }
-  return $contact_number;
-}
-
-function convert_date($date, $format): string
-{
-  return date($format, strtotime($date));
-}
-
-function calc_totals()
-{
-  foreach ($_SESSION['cart']['item'] as $item) {
-    $_SESSION['cart']['total_price'] += $item['subtotal'];
-    $_SESSION['cart']['total_quantity'] += $item['quantity'];
-  }
-}
-
-//カレンダー
-
-// タイムゾーンを日本に変更
-date_default_timezone_set('Asia/Tokyo');
-// 曜日表示用の配列
-$weekday = array('日', '月', '火', '水', '木', '金', '土',);
-// $weekday = array('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat',);
-
-// GETで基準日のデータを受け取れたら値を変数へ格納
-$refYear = isset($_GET['refYear']) ? $_GET['refYear'] : '';
-$refMonth = isset($_GET['refMonth']) ? $_GET['refMonth'] : '';
-
-// 今日のオブジェクトを生成
-$objToday = new DateTime();
-
-// 基準日のオブジェクトを生成
-if (empty($refYear) && empty($refMonth)) {
-  $objRefDate = new DateTime();
-} else {
-  $objRefDate = new DateTime("$refYear-$refMonth-1");
-}
-// 年月日をそれぞれ変数へ格納
-$thisYear = $objRefDate->format('Y');
-$thisMonth = $objRefDate->format('m');
-$thisDate = $objRefDate->format('j');
-// 表示用の月を変数へ格納
-$thisDisplayMonth_num = $objRefDate->format('n');
-$thisDisplayMonth_str = $objRefDate->format('F');
-// 曜日を取得
-$thisWeekDay = $objRefDate->format('w');
-// 当月の総日数を取得
-$thisFullDays = $objRefDate->format('t');
-
-// 基準日を元に初日のオブジェクトを生成
-$objFirstDateTime = new DateTime("$thisYear-$thisMonth-1");
-// 初日の曜日を取得
-$thisFirstWeekDay = $objFirstDateTime->format('w');
-
-// 基準日を元に最終日のオブジェクトを生成
-$objLastDateTime = new DateTime("$thisYear-$thisMonth-$thisFullDays");
-// 最終日の曜日を取得
-$thisLastWeekDay = $objLastDateTime->format('w');
-
-// 前月のオブジェクトを生成
-$objPrevMonth = new DateTime("$thisYear-$thisMonth-1 -1 Months");
-// 次月のオブジェクトを生成
-$objNextMonth = new DateTime("$thisYear-$thisMonth-1 +1 Months");
