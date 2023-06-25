22f2f582eb9e69bee558e15cc7e5ed9ec7e6ec3b
taniyama0922 <136245683+taniyama0922@users.noreply.github.com>
2023-06-25 22:36:46 +0900

    Update index.php

--- index.php
+++ index.php
@@ -15,10 +15,10 @@
     <div id="container">
 
         <header class="header">
-            <h1 class="header__logo"><a href="index.php"><img src="image/camplogo.svg" alt="foreach campground"></a></h1>
+            <h1 class="header__logo"><a href="#"><img src="image/camplogo.svg" alt="foreach campground"></a></h1>
             <nav id="g-nav">
                 <ul class="nav">
-                    <li class="g-nav__item"><a href="#">予約</a></li>
+                    <li class="g-nav__item"><a href="reserve.php">予約</a></li>
                     <li class="g-nav__item"><a href="shop.php">オンラインショップ</a></li>
                 </ul>
             </nav>
@@ -30,18 +30,18 @@
                 <section id="mv">
                     <div class="mv__text">
                         <h1 class="mv__copy">福岡にあるリゾート融合体</h1>
-                        <p class="mv__lead">QUIET&nbsp;PLACE<br>AND&nbsp;THE<br>STARRY&nbsp;SKY</p>
+                        <p class="mv__lead">QUIET PLACE<br>AND THE<br>STARRY SKY</p>
                     </div>
                 </section>
 
                 <section id="news01" class="contents">
                     <div class="news_page_description">
-                        <h1 class="title-style">
+                        <h2 class="title-style">
                             01
                             <p class="title-border-right">NEWS</p>
-                        </h1>
+                        </h2>
                         <p>イベント情報など<br>最新の情報をお届けいたします。</p>
-                        <a href="news.php" class="btn">READ&nbsp;MORE</a>
+                        <a href="news.php" class="btn">READ MORE</a>
                     </div>
                     <ul class="news__item">
                         <li><a href="#">
@@ -56,38 +56,38 @@
                     </ul>
                 </section>
 
-                <section id="facility" class="contents">
+                <section id="facility02" class="contents">
                     <div class="page_description right_flow">
-                        <h1 class="title-style">
+                        <h2 class="title-style">
                             02
                             <p class="title-border-left">FACILITY</p>
-                        </h1>
+                        </h2>
                         <p>サイトや料金など、<br>快適にキャンプをお楽しみ頂くための<br>施設をご紹介いたします。</p>
                         <a href="facility.php" class="btn btn_r">READ MORE</a>
                     </div>
                     <figure class="contents_img"><img src="image/tent1.jpg" alt="テント"></figure>
                 </section>
                 
-                <section id="onlinestore" class="contents">
+                <section id="onlinestore03" class="contents">
                     <div class="page_description">
-                        <h1 class="title-style">
+                        <h2 class="title-style">
                             03
-                            <p class="title-border-right">ONLINE&nbsp;SHOP</p>
-                        </h1>
+                            <p class="title-border-right">STORE</p>
+                        </h2>
                         <p>キャンプギア、アパレル用品の購入はこちらから！<br>限定商品も取り扱っていますので<br>是非ご覧ください。</p>
-                        <a href="shop.php" class="btn">READ&nbsp;MORE</a>
+                        <a href="shop.php" class="btn">READ MORE</a>
                         </div>
                         <figure class="contents_img"><img src="image/mug.jpg" alt="マグカップ"></figure>
                 </section>
 
-                <section id="access" class="contents">
+                <section id="access04" class="contents">
                     <div class="page_description right_flow">
-                        <h1 class="title-style">
+                        <h2 class="title-style">
                             04
                             <p class="title-border-left">ACCESS</p>
-                        </h1>
+                        </h2>
                         <p>foreach camp ground<br>〒888-8888<br>福岡県福岡市東区888-88<br>0493-81-6166</p>
-                        <a href="access.php" class="btn btn_r">READ&nbsp;MORE</a>
+                        <a href="#" class="btn btn_r">READ MORE</a>
                         </div>
                         <figure class="contents_img">googlemap</figure>
                 </section>
@@ -98,21 +98,21 @@
         <footer class="footer">
         <div class="ft">
         <ul class="ft__ul">
-        <li class="ft__logo"><a href="index.php"><img src="image/camplogo.svg" alt="foreach campground"></a></li>
-        <li class="ft__add"><span class="ft__name">foreach&nbsp;camp&nbsp;ground</span></li>
+        <li class="ft__logo"><a href="#"><img src="image/camplogo.svg" alt="foreach campground"></a></li>
+        <li class="ft__add"><span class="ft__name">foreach camp ground</span></li>
         <li class="ft__add">〒888-8888</li>
         <li class="ft__add">福岡県福岡市東区888-88</li>
         <li class="ft__add">0493-81-6166</li>
         </ul>
             <ul class="ft_links_ul">
-                <li class="ft_links_li"><a href="access.php">アクセス</a></li>
+                <li class="ft_links_li"><a href="#">アクセス</a></li>
                 <li class="ft_links_li"><a href="news.php">お知らせ</a></li>
                 <li class="ft_links_li"><a href="facility.php">施設紹介</a></li>
-                <li class="ft_links_li"><a href="book.php">予約</a></li>
+                <li class="ft_links_li"><a href="reserve.php">予約</a></li>
                 <li class="ft_links_li"><a href="shop.php">オンラインストア</a></li>
             </ul>
             <ul class="ft_links_ul">
-                <li class="ft_links_li"><a href="#">会社概要・拠点情報</a></li>
+                <li class="ft_links_li"><a href="contact.php">お問い合わせ</a></li>
                 <li class="ft_links_li"><a href="#">事業情報</a></li>
                 <li class="ft_links_li"><a href="#">採用情報</a></li>
                 <li class="ft_links_li"><a href="#">個人情報保護方針</a></li>
