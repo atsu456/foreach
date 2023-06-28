a062e67aebaea260402c4987e2f20261f9d215da
taniyama0922 <136245683+taniyama0922@users.noreply.github.com>
2023-06-28 23:05:19 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+if(!isset($_SESSION['login'])){
+	header('Location:login.php');
+}
+
+require_once '../func/func.php';
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -6,7 +15,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>index</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="https://use.typekit.net/pke3ujd.css">
     <script src="https://kit.fontawesome.com/0fb73e8725.js" crossorigin="anonymous"></script>
 </head>
@@ -15,7 +24,7 @@
     <div id="container">
 
         <header class="header">
-            <h1 class="header__logo"><a href="#"><img src="image/camplogo.svg" alt="foreach campground"></a></h1>
+            <h1 class="header__logo"><a href="#"><img src="../image/camplogo.svg" alt="foreach campground"></a></h1>
             <nav id="g-nav">
                 <ul class="nav">
                     <li class="g-nav__item"><a href="reserve.php">予約</a></li>
@@ -24,81 +33,25 @@
             </nav>
         </header>
 
-        <main>
-            <article>
-
-                <section id="mv">
-                    <div class="mv__text">
-                        <h1 class="mv__copy">福岡にあるリゾート融合体</h1>
-                        <p class="mv__lead">QUIET PLACE<br>AND THE<br>STARRY SKY</p>
-                    </div>
-                </section>
-
-                <section id="news01" class="contents">
-                    <div class="news_page_description">
-                        <h2 class="title-style">
-                            01
-                            <p class="title-border-right">NEWS</p>
-                        </h2>
-                        <p>イベント情報など<br>最新の情報をお届けいたします。</p>
-                        <a href="news.php" class="btn">READ MORE</a>
-                    </div>
-                    <ul class="news__item">
-                        <li><a href="#">
-                                <figure class="img"><img src="image/rantan.jpg" alt="ランタン"></figure>
-                            </a></li>
-                        <li><a href="#">
-                                <figure class="img"><img src="image/cup.jpg" alt="マグカップ"></figure>
-                            </a></li>
-                        <li><a href="#">
-                                <figure class="img"><img src="image/fire.jpg" alt="焚き火"></figure>
-                            </a></li>
-                    </ul>
-                </section>
-
-                <section id="facility02" class="contents">
-                    <div class="page_description right_flow">
-                        <h2 class="title-style">
-                            02
-                            <p class="title-border-left">FACILITY</p>
-                        </h2>
-                        <p>サイトや料金など、<br>快適にキャンプをお楽しみ頂くための<br>施設をご紹介いたします。</p>
-                        <a href="facility.php" class="btn btn_r">READ MORE</a>
-                    </div>
-                    <figure class="contents_img"><img src="image/tent1.jpg" alt="テント"></figure>
-                </section>
-                
-                <section id="onlinestore03" class="contents">
-                    <div class="page_description">
-                        <h2 class="title-style">
-                            03
-                            <p class="title-border-right">STORE</p>
-                        </h2>
-                        <p>キャンプギア、アパレル用品の購入はこちらから！<br>限定商品も取り扱っていますので<br>是非ご覧ください。</p>
-                        <a href="shop.php" class="btn">READ MORE</a>
-                        </div>
-                        <figure class="contents_img"><img src="image/mug.jpg" alt="マグカップ"></figure>
-                </section>
-
-                <section id="access04" class="contents">
-                    <div class="page_description right_flow">
-                        <h2 class="title-style">
-                            04
-                            <p class="title-border-left">ACCESS</p>
-                        </h2>
-                        <p>foreach camp ground<br>〒888-8888<br>福岡県福岡市東区888-88<br>0493-81-6166</p>
-                        <a href="#" class="btn btn_r">READ MORE</a>
-                        </div>
-                        <figure class="contents_img">googlemap</figure>
-                </section>
-
-            </article>
+        <main class="admin_main">
+		<article id="admin">
+			<h1>管理メニュー</h1>
+			<p>ようこそ<?= ($_SESSION['login_name']) ?>さん</p>
+			<ul>
+				<li><a href="users/">管理：ユーザ</a></li>
+				<li><a href="contact/">管理：お問い合わせ</a></li>
+				<li><a href="types/">管理：お問い合わせ種類</a></li>
+				<li><a href="orders/">管理：受注管理</a></li>
+				<li><a href="products/">管理：商品管理</a></li>
+				<li><a href="logout.php">ログアウト</a></li>
+			</ul>
+		</article>
         </main>
 
         <footer class="footer">
         <div class="ft">
         <ul class="ft__ul">
-        <li class="ft__logo"><a href="#"><img src="image/camplogo.svg" alt="foreach campground"></a></li>
+        <li class="ft__logo"><a href="#"><img src="../image/camplogo.svg" alt="foreach campground"></a></li>
         <li class="ft__add"><span class="ft__name">foreach camp ground</span></li>
         <li class="ft__add">〒888-8888</li>
         <li class="ft__add">福岡県福岡市東区888-88</li>
@@ -106,17 +59,17 @@
         </ul>
             <ul class="ft_links_ul">
                 <li class="ft_links_li"><a href="#">アクセス</a></li>
-                <li class="ft_links_li"><a href="news.php">お知らせ</a></li>
-                <li class="ft_links_li"><a href="facility.php">施設紹介</a></li>
-                <li class="ft_links_li"><a href="reserve.php">予約</a></li>
-                <li class="ft_links_li"><a href="shop.php">オンラインストア</a></li>
+                <li class="ft_links_li"><a href="#">お知らせ</a></li>
+                <li class="ft_links_li"><a href="#">施設紹介</a></li>
+                <li class="ft_links_li"><a href="#">予約</a></li>
+                <li class="ft_links_li"><a href="#">オンラインストア</a></li>
             </ul>
             <ul class="ft_links_ul">
-                <li class="ft_links_li"><a href="contact.php">お問い合わせ</a></li>
+                <li class="ft_links_li"><a href="#">会社概要・拠点情報</a></li>
                 <li class="ft_links_li"><a href="#">事業情報</a></li>
                 <li class="ft_links_li"><a href="#">採用情報</a></li>
                 <li class="ft_links_li"><a href="#">個人情報保護方針</a></li>
-                <li class="ft_links_li"><a href="#">ソーシャルメディアポリシー</a></li>
+                <li class="ft_links_li"><a href="contact.php">お問い合わせ</a></li>
             </ul>
             </div>
             <div class="ft_snsLinks">
