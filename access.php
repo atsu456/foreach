48543cbef64f1cc87e975b47b90b2197c5e51e4c
tanitama <h2.sep22@icloud.com>
2023-07-08 20:46:23 +0900

    access,css追加

--- access.php
+++ access.php
@@ -21,41 +21,42 @@
                         <h1 class="mv__lead">ACCESS</h1>
                     </div>
                 </section>
-                <section id="add" class="contents">
-                    <ul class="accsess__ul">
-                        <li class="accsess__add">foreach camp ground</li>
-                        <li class="accsess__add">〒888-8888</li>
-                        <li class="accsess__add">福岡県福岡市東区888-88</li>
-                        <li class="accsess__add">XXXX-XX-XXXX</li>
-                        <li class="accsess__add">営業時間：10:00〜18:00 </li>
+                <section id="access__add">
+                    <ul class="access__ul">
+                        <li class="access__li">foreach camp ground</li>
+                        <li class="access__li">〒888-8888</li>
+                        <li class="access__li">福岡県福岡市東区888-88</li>
+                        <li class="access__li">XXXX-XX-XXXX</li>
+                        <li class="access__li">営業時間：10:00〜18:00 </li>
                     </ul>
                 </section>
-
+            </article>
+            <article class="contents">
                 <section id="car">
                     <p>車でお越しの場合</p>
                     <p>福岡市都市高速「東ランプ」から約8km（約20分）</p>
                 </section>
                 <section id="point">
-                    <p>おすすめポイント</p>
+                    <p class="point">- Recommended Points -<br><span class="small">おすすめポイント</span></p>
                     <div>
-                    <img src="image/benziad-ir56ZAc3Z0s-unsplash.jpg" alt="">
-                    <p>スーパーマーケット&nbsp;イーストマート</p>
-                    <p>車で約5分。<br>豊富な品揃えのスーパーが近距離に！<br>BBQの食材や、追加の買い物にも便利です。</p>
+                        <img class="access_img" src="image/benziad-ir56ZAc3Z0s-unsplash.jpg">
+                        <p class="access_big">スーパーマーケット&nbsp;イーストマート</p>
+                        <p>車で約5分。<br>豊富な品揃えのスーパーが近距離に！<br>BBQの食材や、追加の買い物にも便利です。</p>
                     </div>
                     <div>
-                    <img src="image/ralph-ravi-kayden-CsqHFS6ZXfM-unsplash.jpg" alt="">
-                    <p>源泉掛け流し温泉&nbsp;東の湯</p>
-                    <p>車で約15分。<br>キャンプを堪能した後、天然温泉で疲れをとりませんか。</p>
+                        <img class="access_img" src="image/ralph-ravi-kayden-CsqHFS6ZXfM-unsplash.jpg" alt="">
+                        <p class="access_big">源泉掛け流し温泉&nbsp;東の湯</p>
+                        <p>車で約15分。<br>キャンプを堪能した後、天然温泉で疲れをとりませんか。</p>
                     </div>
                     <div>
-                    <img src="image/drink-g4db5af7fb_1920.jpg" alt="">
-                    <p>cafe&nbsp;ini</p>
-                    <p>車で約25分。<br>地元の食材を使用した軽食あり。<br>人気のカフェです。</p>
+                        <img class="access_img" src="image/drink-g4db5af7fb_1920.jpg" alt="">
+                        <p class="access_big">cafe&nbsp;ini</p>
+                        <p>車で約25分。<br>地元の食材を使用した軽食あり。人気のカフェです。</p>
                     </div>
                     <div>
-                    <img src="image/zip-line-gb5fee0b00_1920.jpg" alt="">
-                    <p>フォレストアドベンチャー</p>
-                    <p>車で約30分。<br>フォレストアドベンチャー」では、森林アスレチックで汗を流すことができます。お子さま向けのネットコースも完備。</p>
+                        <img class="access_img" src="image/zip-line-gb5fee0b00_1920.jpg" alt="">
+                        <p class="access_big">フォレストアドベンチャー</p>
+                        <p>車で約30分。<br>フォレストアドベンチャー」では、森林アスレチックで汗を流すことができます。<br>お子さま向けのネットコースも完備。</p>
                     </div>
                 </section>
 
