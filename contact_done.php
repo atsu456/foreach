7f46fe62a40278fe71b69fe1fcfc5064c30e5b3a
tanitama <h2.sep22@icloud.com>
2023-07-12 15:51:31 +0900

    kousin

--- contact_done.php
+++ contact_done.php
@@ -28,14 +28,7 @@ $contact_number = numbers(64);
 <body id="top">
     <div id="container">
 
-        <header class="header">
-            <h1 class="header__logo"><a href="#"><img src="image/camplogo.svg" alt="foreach campground"></a></h1>
-            <nav id="g-nav">
-                <ul class="nav">
-                    <li class="g-nav__item"><a href="reserve.php">予約</a></li>
-                    <li class="g-nav__item"><a href="onlineshop.php">オンラインショップ</a></li>
-                </ul>
-            </nav>
+    <?php require_once('header.html'); ?>
         </header>
         <main>
             <article>
@@ -76,39 +69,9 @@ $contact_number = numbers(64);
                 </div>
             </article>
         </main>
-        <footer class="footer">
-            <div class="ft">
-                <ul class="ft__ul">
-                    <li class="ft__logo"><a href="#"><img src="image/camplogo.svg" alt="foreach campground"></a></li>
-                    <li class="ft__add"><span class="ft__name">foreach camp ground</span></li>
-                    <li class="ft__add">〒888-8888</li>
-                    <li class="ft__add">福岡県福岡市東区888-88</li>
-                    <li class="ft__add">0493-81-6166</li>
-                </ul>
-                <ul class="ft_links_ul">
-                    <li class="ft_links_li"><a href="#">アクセス</a></li>
-                    <li class="ft_links_li"><a href="#">お知らせ</a></li>
-                    <li class="ft_links_li"><a href="#">施設紹介</a></li>
-                    <li class="ft_links_li"><a href="#">予約</a></li>
-                    <li class="ft_links_li"><a href="#">オンラインストア</a></li>
-                </ul>
-                <ul class="ft_links_ul">
-                    <li class="ft_links_li"><a href="#">会社概要・拠点情報</a></li>
-                    <li class="ft_links_li"><a href="#">事業情報</a></li>
-                    <li class="ft_links_li"><a href="#">採用情報</a></li>
-                    <li class="ft_links_li"><a href="#">個人情報保護方針</a></li>
-                    <li class="ft_links_li"><a href="#">ソーシャルメディアポリシー</a></li>
-                </ul>
-            </div>
-            <div class="ft_snsLinks">
-                <ul class="ft_snsLinks_ul">
-                    <li><a href="#"><i class="fa-brands fa-square-facebook"></i></a></li>
-                    <li><a href="#"><i class="fa-brands fa-square-twitter"></i></a></li>
-                    <li><a href="#"><i class="fa-brands fa-square-instagram"></i></a></li>
-                </ul>
-            </div>
-            <div class="ft_copyright">©2023 foreach campground</div>
-        </footer>
+        <?php require_once('footer.html'); ?>
+        <script src="js/jquery-3.3.1.min.js"></script>
+        <script src="js/menu.js"></script>
 </body>
 
 </html>
